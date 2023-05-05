579b4401ee6cf5de9045b30c8821fd564df5a69d
MOHAMMED TAHALLA <111022553+SimoAs@users.noreply.github.com>
2023-05-05 23:11:12 +0100

    Commit

--- resources/views/Track/tracking.blade.php
+++ resources/views/Track/tracking.blade.php
@@ -32,7 +32,7 @@
 
       <div class="navbar-brand">
         <a class="navbar-brand-logo" href="#top">
-          <img src="images/logo.png" alt="logo">
+          <img style="width: 50%;" src="images/logo.png" alt="logo">
         </a>
       </div>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
@@ -42,26 +42,8 @@
       <div class="collapse navbar-collapse justify-content-around" id="navbarNav">
         <ul class="navbar-nav menu-navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="#top">Home</a>
+          <a class="nav-link" href="{{ url('/') }}">Home</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#tracker">Tracker</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#services">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#blog">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#reviews">Reviews</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link learn-more-btn" href="#contact">Contact</a>
-          </li>
-        </ul>
       </div>
     </div>
   </nav>
@@ -71,13 +53,12 @@
   <div id="top" class="header">
     <div class="container header-container">
       <div class="d-none d-lg-block col-lg-6  header-img-section">
-        <img src="https://via.placeholder.com/525x400/dddddd/068DB4" class="img-fluid" alt="banner">
+        <img style="width: 100%" src="images/SUPRATOURS.jpg" class="img-fluid" alt="banner">
       </div>
       <div class="col-lg-5 offset-lg-1 col-sm-12 header-title-section">
         <p class="header-subtitle">Order Delivery Tracker</p>
-        <h1 class="header-title">Keep your online shopping organized</h1>
-        <p class="header-title-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis vestibulum ante,
-          id gravida sem. Nam dictum ornare dictum. Sed congue accumsan arcu quis consectetur.</p>
+        <h1 class="header-title">SUPRATOURS</h1>
+        <p class="header-title-text">SupraTours est une société de transport basée au Maroc qui fournit des services de bus interurbains à diverses destinations dans le pays.</p>
         <div class="learn-more-btn-section">
           <a class="nav-link learn-more-btn btn-invert" href="#tracker">Track Your Order</a>
         </div>
@@ -99,7 +80,7 @@
               <div class="form-group">
                 <input type="text" class="form-control" id="order-id" name="TrackingCode" value="{{ $TrackingCode }}" placeholder="Order Id">
               </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
+              <button type="submit" class="btn btn-primary">Suiver</button>
             </form>
           </div>
         </div>
@@ -206,7 +187,7 @@
                                 <i class="fas fa-street-view"></i>
                               </div>
                               <div class="tracking-date">{{ mb_substr($Tracking->DateRecption, 0, -5, 'UTF-8') }}<span>{{ mb_substr($Tracking->DateRecption, +10) }} PM</span></div>
-                              <div class="tracking-content">{{ $Tracking->Destination }}<span>Colis Livré le : {{ $Tracking->DateCloture }} PM</span></div>
+                              <div class="tracking-content">{{ $Tracking->Destination }}<span>@if($Tracking->DateCloture == "") Colis Dans L'agence @else Colis Livré le : {{ $Tracking->DateCloture }} PM @endif</span></div>
                             </div>
                             @else
                             <div class="tracking-item">
@@ -257,57 +238,7 @@
 
 
   <!-- footer Start -->
-  <section class="footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-3 col-md-6">
-          <div class="footer-widget">
-            <img src="images/logo-white.png" alt="logo" />
-            <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. libero at, fermentum posuere quam.
-            </p>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-          <div class="footer-widget">
-            <h5>Quick Links</h5>
-            <ul>
-              <li><a href="#"><i class="fas fa-angle-right"></i> Services</a></li>
-              <li><a href="#"><i class="fas fa-angle-right"></i> Tracker</a></li>
-              <li><a href="#"><i class="fas fa-angle-right"></i> Contact Us</a></li>
-              <li><a href="#"><i class="fas fa-angle-right"></i> Reviews</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-          <div class="footer-widget">
-            <h5>Address</h5>
-            <p class="mt-3"><i class="fas fa-map-marker-alt"></i> Street 10, 3rd cross<br>
-              Koramagala<br>
-              0123456789<br>
-              yourmail@yourdomain.com</p>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-          <div class="footer-widget">
-            <h5>Social Connect</h5>
-            <div class="social-icons">
-              <a href="#"><i class="fab fa-facebook"></i></a>
-              <a href="#"><i class="fab fa-twitter"></i></a>
-              <a href="#"><i class="fab fa-linkedin"></i></a>
-              <a href="#"><i class="fab fa-instagram"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="footer-copyright">
-            © 2020 KonnectCode All Rights Reserved.
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+
   <!-- footer End -->
 
   <!-- jQuery Min JS -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <title>TrakShip | Delivery Tracking Template</title>
+  <title>SupraTours | Delivery Tracking</title>
   <!-- Plugins CSS -->
   <link href="css/plugins.css" rel="stylesheet">
 
@@ -32,7 +32,7 @@
 
       <div class="navbar-brand">
         <a class="navbar-brand-logo" href="#top">
-          <img src="images/logo.png" alt="logo">
+          <img style="width: 50%;" src="images/logo.png" alt="logo">
         </a>
       </div>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
@@ -42,24 +42,7 @@
       <div class="collapse navbar-collapse justify-content-around" id="navbarNav">
         <ul class="navbar-nav menu-navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" href="#top">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#tracker">Tracker</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#services">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#blog">Blog</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#reviews">Reviews</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link learn-more-btn" href="#contact">Contact</a>
+            <a class="nav-link" href="{{ url('/') }}">Home</a>
           </li>
         </ul>
       </div>
@@ -71,13 +54,12 @@
   <div id="top" class="header">
     <div class="container header-container">
       <div class="d-none d-lg-block col-lg-6  header-img-section">
-        <img src="https://via.placeholder.com/525x400/dddddd/068DB4" class="img-fluid" alt="banner">
+        <img style="width: 100%" src="images/SUPRATOURS.jpg" class="img-fluid" alt="banner">
       </div>
       <div class="col-lg-5 offset-lg-1 col-sm-12 header-title-section">
         <p class="header-subtitle">Order Delivery Tracker</p>
-        <h1 class="header-title">Keep your online shopping organized</h1>
-        <p class="header-title-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut quis vestibulum ante,
-          id gravida sem. Nam dictum ornare dictum. Sed congue accumsan arcu quis consectetur.</p>
+        <h1 class="header-title">SUPRATOURS</h1>
+        <p class="header-title-text">SupraTours est une société de transport basée au Maroc qui fournit des services de bus interurbains à diverses destinations dans le pays.</p>
         <div class="learn-more-btn-section">
           <a class="nav-link learn-more-btn btn-invert" href="#tracker">Track Your Order</a>
         </div>
@@ -99,7 +81,7 @@
               <div class="form-group">
                 <input type="text" class="form-control" id="order-id" name="TrackingCode"  placeholder="Order Id">
               </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
+              <button type="submit" class="btn btn-primary">Suiver</button>
             </form>
           </div>
         </div>
@@ -125,59 +107,7 @@
   <!-- Tracker End -->
 
 
-  <!-- footer Start -->
-  <section class="footer">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-3 col-md-6">
-          <div class="footer-widget">
-            <img src="images/logo-white.png" alt="logo" />
-            <p class="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. libero at, fermentum posuere quam.
-            </p>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-          <div class="footer-widget">
-            <h5>Quick Links</h5>
-            <ul>
-              <li><a href="#"><i class="fas fa-angle-right"></i> Services</a></li>
-              <li><a href="#"><i class="fas fa-angle-right"></i> Tracker</a></li>
-              <li><a href="#"><i class="fas fa-angle-right"></i> Contact Us</a></li>
-              <li><a href="#"><i class="fas fa-angle-right"></i> Reviews</a></li>
-            </ul>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-          <div class="footer-widget">
-            <h5>Address</h5>
-            <p class="mt-3"><i class="fas fa-map-marker-alt"></i> Street 10, 3rd cross<br>
-              Koramagala<br>
-              0123456789<br>
-              yourmail@yourdomain.com</p>
-          </div>
-        </div>
-        <div class="col-lg-3 col-md-6">
-          <div class="footer-widget">
-            <h5>Social Connect</h5>
-            <div class="social-icons">
-              <a href="#"><i class="fab fa-facebook"></i></a>
-              <a href="#"><i class="fab fa-twitter"></i></a>
-              <a href="#"><i class="fab fa-linkedin"></i></a>
-              <a href="#"><i class="fab fa-instagram"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-lg-12">
-          <div class="footer-copyright">
-            © 2020 KonnectCode All Rights Reserved.
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <!-- footer End -->
+
 
   <!-- jQuery Min JS -->
   <script src="js/jquery-min.js"></script>
